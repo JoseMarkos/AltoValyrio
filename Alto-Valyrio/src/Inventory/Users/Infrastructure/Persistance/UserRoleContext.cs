67be593830efbe63cba1b1e8bf15fc529e10b3f3
Marcos Culajay <jkmarcosb@gmail.com>
2020-09-04 22:35:39 -0600

    adding empty user role context

---
+++ Alto-Valyrio/src/Inventory/Users/Infrastructure/Persistance/UserRoleContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alto_Valyrio.src.Inventory.Users.Infrastructure.Persistance
+{
+    class UserRoleContext : DBContext
+    {
+    }
+}
